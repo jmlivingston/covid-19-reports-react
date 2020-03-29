80bba26081e8d2952f7f76894d62d8da57fa571a
John Livingston <jmlivingston@gmail.com>
2020-03-28 19:12:55 -0700

    Updates

---
+++ precache-manifest.812d68c89d10ba8091b354dfc75262fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131bfef4965aafee9e71ee567b6427f6",
+    "url": "/covid-19-reports-react/index.html"
+  },
+  {
+    "revision": "5601f129bace7d1aed7b",
+    "url": "/covid-19-reports-react/static/css/main.147903f6.chunk.css"
+  },
+  {
+    "revision": "0d4a4c44ce4b27a55864",
+    "url": "/covid-19-reports-react/static/js/2.130cd25e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-reports-react/static/js/2.130cd25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5601f129bace7d1aed7b",
+    "url": "/covid-19-reports-react/static/js/main.82cbcb15.chunk.js"
+  },
+  {
+    "revision": "650f90462f03aa3cfa73",
+    "url": "/covid-19-reports-react/static/js/runtime-main.d0b00341.js"
+  }
+]);
