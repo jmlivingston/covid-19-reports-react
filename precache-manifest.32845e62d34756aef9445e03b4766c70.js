590b2ca096bd9e69e0d99551233114b01ea71699
John Livingston <jmlivingston@gmail.com>
2020-03-29 18:55:33 -0700

    Updates

---
+++ precache-manifest.32845e62d34756aef9445e03b4766c70.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599cfb25379a8e3e10fd4d155cb70d0d",
+    "url": "/covid-19-reports-react/index.html"
+  },
+  {
+    "revision": "5fc96d5cd2723c57eff7",
+    "url": "/covid-19-reports-react/static/css/4.0f2d7d6b.chunk.css"
+  },
+  {
+    "revision": "3431f4a3b7f132fe460d",
+    "url": "/covid-19-reports-react/static/css/5.0f2d7d6b.chunk.css"
+  },
+  {
+    "revision": "4b51738f1a266cf70956",
+    "url": "/covid-19-reports-react/static/css/main.2e96036b.chunk.css"
+  },
+  {
+    "revision": "1b1a21bc56adeb13f559",
+    "url": "/covid-19-reports-react/static/js/0.2ec96696.chunk.js"
+  },
+  {
+    "revision": "5492d3f25335ddb3d7b9",
+    "url": "/covid-19-reports-react/static/js/3.56a04625.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-reports-react/static/js/3.56a04625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc96d5cd2723c57eff7",
+    "url": "/covid-19-reports-react/static/js/4.56416d90.chunk.js"
+  },
+  {
+    "revision": "3431f4a3b7f132fe460d",
+    "url": "/covid-19-reports-react/static/js/5.be0c9f42.chunk.js"
+  },
+  {
+    "revision": "30d4c73d4c4fd8c2afba",
+    "url": "/covid-19-reports-react/static/js/6.96ac726a.chunk.js"
+  },
+  {
+    "revision": "da4f05ca42aea1ef3f7c",
+    "url": "/covid-19-reports-react/static/js/7.082b4a23.chunk.js"
+  },
+  {
+    "revision": "678c1d01c933c49ebc85",
+    "url": "/covid-19-reports-react/static/js/8.1054d6ff.chunk.js"
+  },
+  {
+    "revision": "4b51738f1a266cf70956",
+    "url": "/covid-19-reports-react/static/js/main.d3125948.chunk.js"
+  },
+  {
+    "revision": "1c16030add1766c44f5a",
+    "url": "/covid-19-reports-react/static/js/runtime-main.2877d754.js"
+  }
+]);
