f005971eb2d5d0e32ba6d12bf7210f4e4d13d2cb
John Livingston <jmlivingston@gmail.com>
2020-03-29 19:31:10 -0700

    Updates

---
+++ precache-manifest.26c0e8761a682b1ab29a3fd0e5516ed7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853c3c216c43767ea57a0bf79b8724fc",
+    "url": "/covid-19-reports-react/index.html"
+  },
+  {
+    "revision": "7bda4cfe49781b5ab5db",
+    "url": "/covid-19-reports-react/static/css/1.0f2d7d6b.chunk.css"
+  },
+  {
+    "revision": "36961df13db57f84da81",
+    "url": "/covid-19-reports-react/static/css/7.0f2d7d6b.chunk.css"
+  },
+  {
+    "revision": "4dca985f3e24e694f4ee",
+    "url": "/covid-19-reports-react/static/css/8.0f2d7d6b.chunk.css"
+  },
+  {
+    "revision": "cec80ccde353e2cb3045",
+    "url": "/covid-19-reports-react/static/css/main.2e96036b.chunk.css"
+  },
+  {
+    "revision": "06aec92a3f862191df95",
+    "url": "/covid-19-reports-react/static/js/0.6d4509b4.chunk.js"
+  },
+  {
+    "revision": "7bda4cfe49781b5ab5db",
+    "url": "/covid-19-reports-react/static/js/1.f41c5e97.chunk.js"
+  },
+  {
+    "revision": "150d4c574fc6ca12b9ba",
+    "url": "/covid-19-reports-react/static/js/10.0a00350f.chunk.js"
+  },
+  {
+    "revision": "0f6679ce80c5c9576aa5",
+    "url": "/covid-19-reports-react/static/js/11.ac99ed45.chunk.js"
+  },
+  {
+    "revision": "8c511f923cdce75f052a",
+    "url": "/covid-19-reports-react/static/js/2.6ed4b964.chunk.js"
+  },
+  {
+    "revision": "e9e164edcfc0141bea96",
+    "url": "/covid-19-reports-react/static/js/3.64430c75.chunk.js"
+  },
+  {
+    "revision": "724999898df7967fd8ee",
+    "url": "/covid-19-reports-react/static/js/6.f93b67a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-reports-react/static/js/6.f93b67a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36961df13db57f84da81",
+    "url": "/covid-19-reports-react/static/js/7.cdd7b94b.chunk.js"
+  },
+  {
+    "revision": "4dca985f3e24e694f4ee",
+    "url": "/covid-19-reports-react/static/js/8.72b2524f.chunk.js"
+  },
+  {
+    "revision": "1ada754596fa250e53b7",
+    "url": "/covid-19-reports-react/static/js/9.619f094a.chunk.js"
+  },
+  {
+    "revision": "cec80ccde353e2cb3045",
+    "url": "/covid-19-reports-react/static/js/main.d9250736.chunk.js"
+  },
+  {
+    "revision": "2c96a9bfad4055195219",
+    "url": "/covid-19-reports-react/static/js/runtime-main.82045031.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ecd3e21d25abbdb8c6b16caa2fa582e.js"
+  "/covid-19-reports-react/precache-manifest.26c0e8761a682b1ab29a3fd0e5516ed7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-reports-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
