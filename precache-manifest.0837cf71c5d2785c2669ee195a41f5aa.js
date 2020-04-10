73da6ff6a7c07707bd068513a936d1e1090335e7
John Livingston <john.livingston@upperstrata.com>
2020-04-09 17:41:43 -0700

    Updates

---
+++ precache-manifest.0837cf71c5d2785c2669ee195a41f5aa.js
@@ -0,0 +1,598 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d82f5bef845fc54fb7dc17becddc653",
+    "url": "/covid-19-reports-react/index.html"
+  },
+  {
+    "revision": "5f1895997a2b7b0fa49b",
+    "url": "/covid-19-reports-react/static/css/10.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "c84e3c52ffad36dd14fb",
+    "url": "/covid-19-reports-react/static/css/11.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "c8c1d0b7d72f0405f133",
+    "url": "/covid-19-reports-react/static/css/12.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "8594b07405e0674cae1a",
+    "url": "/covid-19-reports-react/static/css/13.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "ba11829b751c395c7772",
+    "url": "/covid-19-reports-react/static/css/14.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "d1778fd6b380e05747bd",
+    "url": "/covid-19-reports-react/static/css/15.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "eed413824c117e43af8e",
+    "url": "/covid-19-reports-react/static/css/16.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "f5416a1989827b13f0ff",
+    "url": "/covid-19-reports-react/static/css/17.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "8edc864ba686976313c0",
+    "url": "/covid-19-reports-react/static/css/18.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "42c1020b8cf11ef53a6c",
+    "url": "/covid-19-reports-react/static/css/19.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "a2f55d13e03a39c47153",
+    "url": "/covid-19-reports-react/static/css/20.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "d8a9724057f5c47d492c",
+    "url": "/covid-19-reports-react/static/css/21.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "b865a584e1c1cd90b2cb",
+    "url": "/covid-19-reports-react/static/css/22.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "343d456454f943c5e24f",
+    "url": "/covid-19-reports-react/static/css/23.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "e819a9d685946ccdf989",
+    "url": "/covid-19-reports-react/static/css/24.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "40a4dea7574c256bd057",
+    "url": "/covid-19-reports-react/static/css/25.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "8e8b12b8bf5399b8387b",
+    "url": "/covid-19-reports-react/static/css/26.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "b9060c6bf8bc25fbd9d6",
+    "url": "/covid-19-reports-react/static/css/27.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "4a61b620c146ae8781c8",
+    "url": "/covid-19-reports-react/static/css/28.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "23397421ac343163a87a",
+    "url": "/covid-19-reports-react/static/css/29.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "301d8ac64eb20380420a",
+    "url": "/covid-19-reports-react/static/css/30.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "5f9f798e692fd191817e",
+    "url": "/covid-19-reports-react/static/css/31.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "d29d4a9c06e0375fe3f5",
+    "url": "/covid-19-reports-react/static/css/32.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "4f151b86437cddebb638",
+    "url": "/covid-19-reports-react/static/css/33.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "377b83128f95a603f525",
+    "url": "/covid-19-reports-react/static/css/34.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "c90e8455011a686ad20a",
+    "url": "/covid-19-reports-react/static/css/35.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "ae6aa21ba4453497b757",
+    "url": "/covid-19-reports-react/static/css/36.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "73358f85f7089b794f27",
+    "url": "/covid-19-reports-react/static/css/37.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "07387f1efcdec7ac22fc",
+    "url": "/covid-19-reports-react/static/css/38.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "5185358067806b92d787",
+    "url": "/covid-19-reports-react/static/css/39.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "13b7e14d30d65c4a1f1c",
+    "url": "/covid-19-reports-react/static/css/40.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "a23f9b74b77ec9d8ef49",
+    "url": "/covid-19-reports-react/static/css/41.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "d0895f1e9a881c2c94bc",
+    "url": "/covid-19-reports-react/static/css/42.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "c4f664b7530e7ea2aaf0",
+    "url": "/covid-19-reports-react/static/css/43.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "3b1d183377fc7a8b6807",
+    "url": "/covid-19-reports-react/static/css/44.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "d502e1461cf47b311aa2",
+    "url": "/covid-19-reports-react/static/css/45.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "7941bebb9434916f7c86",
+    "url": "/covid-19-reports-react/static/css/46.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "54c7924a1c3a3b8590a7",
+    "url": "/covid-19-reports-react/static/css/47.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "db64aac2ffd361a4b3c8",
+    "url": "/covid-19-reports-react/static/css/48.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "3406fbd5438ee480a950",
+    "url": "/covid-19-reports-react/static/css/49.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "abfedb2133a2129db389",
+    "url": "/covid-19-reports-react/static/css/5.1a0998ea.chunk.css"
+  },
+  {
+    "revision": "121dfc92c5047ac7cff6",
+    "url": "/covid-19-reports-react/static/css/50.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "728b5ee85c27525fda57",
+    "url": "/covid-19-reports-react/static/css/51.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "0ec19afc99d6b5a3346c",
+    "url": "/covid-19-reports-react/static/css/52.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "40a245be52b382a73bc6",
+    "url": "/covid-19-reports-react/static/css/53.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "e0fe9d81e26d2429fe3e",
+    "url": "/covid-19-reports-react/static/css/54.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "af740051e9c79a8581e3",
+    "url": "/covid-19-reports-react/static/css/55.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "c82ded4c57cf99cc5e96",
+    "url": "/covid-19-reports-react/static/css/56.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "31c1e345837932f3132f",
+    "url": "/covid-19-reports-react/static/css/6.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "4dd209a56a2c79b32bb9",
+    "url": "/covid-19-reports-react/static/css/7.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "9d07697813ee527c0e68",
+    "url": "/covid-19-reports-react/static/css/8.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "6f17caf6f95fa167decc",
+    "url": "/covid-19-reports-react/static/css/9.e49e2a53.chunk.css"
+  },
+  {
+    "revision": "a645dd46ecbb99526dd3",
+    "url": "/covid-19-reports-react/static/css/main.d8b07158.chunk.css"
+  },
+  {
+    "revision": "eeaba99b76fcab12ecc8",
+    "url": "/covid-19-reports-react/static/js/0.19f70337.chunk.js"
+  },
+  {
+    "revision": "3002e4f50b1e04863fef",
+    "url": "/covid-19-reports-react/static/js/1.bccb5a71.chunk.js"
+  },
+  {
+    "revision": "5f1895997a2b7b0fa49b",
+    "url": "/covid-19-reports-react/static/js/10.102fcf25.chunk.js"
+  },
+  {
+    "revision": "c84e3c52ffad36dd14fb",
+    "url": "/covid-19-reports-react/static/js/11.f4a1e46c.chunk.js"
+  },
+  {
+    "revision": "c8c1d0b7d72f0405f133",
+    "url": "/covid-19-reports-react/static/js/12.3ec99490.chunk.js"
+  },
+  {
+    "revision": "8594b07405e0674cae1a",
+    "url": "/covid-19-reports-react/static/js/13.b40e7d81.chunk.js"
+  },
+  {
+    "revision": "ba11829b751c395c7772",
+    "url": "/covid-19-reports-react/static/js/14.680251f8.chunk.js"
+  },
+  {
+    "revision": "d1778fd6b380e05747bd",
+    "url": "/covid-19-reports-react/static/js/15.9b75e458.chunk.js"
+  },
+  {
+    "revision": "eed413824c117e43af8e",
+    "url": "/covid-19-reports-react/static/js/16.115d6156.chunk.js"
+  },
+  {
+    "revision": "f5416a1989827b13f0ff",
+    "url": "/covid-19-reports-react/static/js/17.f95f9bc3.chunk.js"
+  },
+  {
+    "revision": "8edc864ba686976313c0",
+    "url": "/covid-19-reports-react/static/js/18.f9837256.chunk.js"
+  },
+  {
+    "revision": "42c1020b8cf11ef53a6c",
+    "url": "/covid-19-reports-react/static/js/19.2c0fe208.chunk.js"
+  },
+  {
+    "revision": "a2f55d13e03a39c47153",
+    "url": "/covid-19-reports-react/static/js/20.d9b1a4e5.chunk.js"
+  },
+  {
+    "revision": "d8a9724057f5c47d492c",
+    "url": "/covid-19-reports-react/static/js/21.b47dd4e8.chunk.js"
+  },
+  {
+    "revision": "b865a584e1c1cd90b2cb",
+    "url": "/covid-19-reports-react/static/js/22.b9922269.chunk.js"
+  },
+  {
+    "revision": "343d456454f943c5e24f",
+    "url": "/covid-19-reports-react/static/js/23.9ee8571a.chunk.js"
+  },
+  {
+    "revision": "e819a9d685946ccdf989",
+    "url": "/covid-19-reports-react/static/js/24.3681ed30.chunk.js"
+  },
+  {
+    "revision": "40a4dea7574c256bd057",
+    "url": "/covid-19-reports-react/static/js/25.f23ffcdd.chunk.js"
+  },
+  {
+    "revision": "8e8b12b8bf5399b8387b",
+    "url": "/covid-19-reports-react/static/js/26.4d3dddaa.chunk.js"
+  },
+  {
+    "revision": "b9060c6bf8bc25fbd9d6",
+    "url": "/covid-19-reports-react/static/js/27.38db8966.chunk.js"
+  },
+  {
+    "revision": "4a61b620c146ae8781c8",
+    "url": "/covid-19-reports-react/static/js/28.587cee0e.chunk.js"
+  },
+  {
+    "revision": "23397421ac343163a87a",
+    "url": "/covid-19-reports-react/static/js/29.a9d5f67f.chunk.js"
+  },
+  {
+    "revision": "301d8ac64eb20380420a",
+    "url": "/covid-19-reports-react/static/js/30.a20d1dc8.chunk.js"
+  },
+  {
+    "revision": "5f9f798e692fd191817e",
+    "url": "/covid-19-reports-react/static/js/31.45ae8bca.chunk.js"
+  },
+  {
+    "revision": "d29d4a9c06e0375fe3f5",
+    "url": "/covid-19-reports-react/static/js/32.119c4e18.chunk.js"
+  },
+  {
+    "revision": "4f151b86437cddebb638",
+    "url": "/covid-19-reports-react/static/js/33.def2f332.chunk.js"
+  },
+  {
+    "revision": "377b83128f95a603f525",
+    "url": "/covid-19-reports-react/static/js/34.b4e64318.chunk.js"
+  },
+  {
+    "revision": "c90e8455011a686ad20a",
+    "url": "/covid-19-reports-react/static/js/35.33e83944.chunk.js"
+  },
+  {
+    "revision": "ae6aa21ba4453497b757",
+    "url": "/covid-19-reports-react/static/js/36.64f6e5da.chunk.js"
+  },
+  {
+    "revision": "73358f85f7089b794f27",
+    "url": "/covid-19-reports-react/static/js/37.e8cecfe2.chunk.js"
+  },
+  {
+    "revision": "07387f1efcdec7ac22fc",
+    "url": "/covid-19-reports-react/static/js/38.214949d9.chunk.js"
+  },
+  {
+    "revision": "5185358067806b92d787",
+    "url": "/covid-19-reports-react/static/js/39.4cae5d24.chunk.js"
+  },
+  {
+    "revision": "ab646c9b01acb08a3a77",
+    "url": "/covid-19-reports-react/static/js/4.5e86636a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-reports-react/static/js/4.5e86636a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b7e14d30d65c4a1f1c",
+    "url": "/covid-19-reports-react/static/js/40.82766404.chunk.js"
+  },
+  {
+    "revision": "a23f9b74b77ec9d8ef49",
+    "url": "/covid-19-reports-react/static/js/41.38e3adaa.chunk.js"
+  },
+  {
+    "revision": "d0895f1e9a881c2c94bc",
+    "url": "/covid-19-reports-react/static/js/42.be980019.chunk.js"
+  },
+  {
+    "revision": "c4f664b7530e7ea2aaf0",
+    "url": "/covid-19-reports-react/static/js/43.bda33f0c.chunk.js"
+  },
+  {
+    "revision": "3b1d183377fc7a8b6807",
+    "url": "/covid-19-reports-react/static/js/44.aca05444.chunk.js"
+  },
+  {
+    "revision": "d502e1461cf47b311aa2",
+    "url": "/covid-19-reports-react/static/js/45.26758896.chunk.js"
+  },
+  {
+    "revision": "7941bebb9434916f7c86",
+    "url": "/covid-19-reports-react/static/js/46.41f3683a.chunk.js"
+  },
+  {
+    "revision": "54c7924a1c3a3b8590a7",
+    "url": "/covid-19-reports-react/static/js/47.d59d3a70.chunk.js"
+  },
+  {
+    "revision": "db64aac2ffd361a4b3c8",
+    "url": "/covid-19-reports-react/static/js/48.259a8f1e.chunk.js"
+  },
+  {
+    "revision": "3406fbd5438ee480a950",
+    "url": "/covid-19-reports-react/static/js/49.655a01e4.chunk.js"
+  },
+  {
+    "revision": "abfedb2133a2129db389",
+    "url": "/covid-19-reports-react/static/js/5.5d1bf07e.chunk.js"
+  },
+  {
+    "revision": "121dfc92c5047ac7cff6",
+    "url": "/covid-19-reports-react/static/js/50.d07d4ae1.chunk.js"
+  },
+  {
+    "revision": "728b5ee85c27525fda57",
+    "url": "/covid-19-reports-react/static/js/51.9b89b3f9.chunk.js"
+  },
+  {
+    "revision": "0ec19afc99d6b5a3346c",
+    "url": "/covid-19-reports-react/static/js/52.ccb84966.chunk.js"
+  },
+  {
+    "revision": "40a245be52b382a73bc6",
+    "url": "/covid-19-reports-react/static/js/53.8fbc3039.chunk.js"
+  },
+  {
+    "revision": "e0fe9d81e26d2429fe3e",
+    "url": "/covid-19-reports-react/static/js/54.8203d74f.chunk.js"
+  },
+  {
+    "revision": "af740051e9c79a8581e3",
+    "url": "/covid-19-reports-react/static/js/55.cad504d1.chunk.js"
+  },
+  {
+    "revision": "c82ded4c57cf99cc5e96",
+    "url": "/covid-19-reports-react/static/js/56.97e6275f.chunk.js"
+  },
+  {
+    "revision": "fe0ba3acb5a134991e17",
+    "url": "/covid-19-reports-react/static/js/57.c82add34.chunk.js"
+  },
+  {
+    "revision": "0963cac8558bf80fa710",
+    "url": "/covid-19-reports-react/static/js/58.ca655e4f.chunk.js"
+  },
+  {
+    "revision": "9af7811e5d6b49b499d0",
+    "url": "/covid-19-reports-react/static/js/59.7afe48b3.chunk.js"
+  },
+  {
+    "revision": "31c1e345837932f3132f",
+    "url": "/covid-19-reports-react/static/js/6.6a33ad04.chunk.js"
+  },
+  {
+    "revision": "2006cf3bc317c28f7c3b",
+    "url": "/covid-19-reports-react/static/js/60.626488d0.chunk.js"
+  },
+  {
+    "revision": "2df4d920b1d3ee5a9a9d",
+    "url": "/covid-19-reports-react/static/js/61.ccaf415c.chunk.js"
+  },
+  {
+    "revision": "885b691387661d912c84",
+    "url": "/covid-19-reports-react/static/js/62.3a21423f.chunk.js"
+  },
+  {
+    "revision": "6f5934e03db8eb6fcbca",
+    "url": "/covid-19-reports-react/static/js/63.61e8d135.chunk.js"
+  },
+  {
+    "revision": "04b9a47653089b5463da",
+    "url": "/covid-19-reports-react/static/js/64.6ecb3645.chunk.js"
+  },
+  {
+    "revision": "6892bcc3d3b614b293d4",
+    "url": "/covid-19-reports-react/static/js/65.bbe8ab7a.chunk.js"
+  },
+  {
+    "revision": "bc035bc67718452fe75f",
+    "url": "/covid-19-reports-react/static/js/66.17348403.chunk.js"
+  },
+  {
+    "revision": "70ac1d2973aa55acc307",
+    "url": "/covid-19-reports-react/static/js/67.487b2f1c.chunk.js"
+  },
+  {
+    "revision": "c4d075fcab7f681583de",
+    "url": "/covid-19-reports-react/static/js/68.0035e243.chunk.js"
+  },
+  {
+    "revision": "1949cf078e178c0bb559",
+    "url": "/covid-19-reports-react/static/js/69.b8812fab.chunk.js"
+  },
+  {
+    "revision": "4dd209a56a2c79b32bb9",
+    "url": "/covid-19-reports-react/static/js/7.a2f63a74.chunk.js"
+  },
+  {
+    "revision": "fe3451cd42a39a9638f5",
+    "url": "/covid-19-reports-react/static/js/70.f8110fa5.chunk.js"
+  },
+  {
+    "revision": "d297590c83c6d20f3ef7",
+    "url": "/covid-19-reports-react/static/js/71.2440fd17.chunk.js"
+  },
+  {
+    "revision": "487ed2a98851e5717cdc",
+    "url": "/covid-19-reports-react/static/js/72.5a758781.chunk.js"
+  },
+  {
+    "revision": "f5a556db2b895a1487dc",
+    "url": "/covid-19-reports-react/static/js/73.7034d89f.chunk.js"
+  },
+  {
+    "revision": "c15bff60710a31dc020c",
+    "url": "/covid-19-reports-react/static/js/74.3acce65e.chunk.js"
+  },
+  {
+    "revision": "6ac466b537925b1669a3",
+    "url": "/covid-19-reports-react/static/js/75.143e528e.chunk.js"
+  },
+  {
+    "revision": "90f73bf38388b54aca51",
+    "url": "/covid-19-reports-react/static/js/76.f0e0a394.chunk.js"
+  },
+  {
+    "revision": "2c1f9ee2cfefca768504",
+    "url": "/covid-19-reports-react/static/js/77.95e616b1.chunk.js"
+  },
+  {
+    "revision": "02a329525c08020efd68",
+    "url": "/covid-19-reports-react/static/js/78.35ab3655.chunk.js"
+  },
+  {
+    "revision": "29fddf4114e2cd4d1d4e",
+    "url": "/covid-19-reports-react/static/js/79.1e97d7af.chunk.js"
+  },
+  {
+    "revision": "9d07697813ee527c0e68",
+    "url": "/covid-19-reports-react/static/js/8.ff13bf82.chunk.js"
+  },
+  {
+    "revision": "28f75490b618869f2a30",
+    "url": "/covid-19-reports-react/static/js/80.6326de4e.chunk.js"
+  },
+  {
+    "revision": "279badff6c36749b0fb9",
+    "url": "/covid-19-reports-react/static/js/81.f7b8b500.chunk.js"
+  },
+  {
+    "revision": "b86ced58f282799a5955",
+    "url": "/covid-19-reports-react/static/js/82.5ad51e21.chunk.js"
+  },
+  {
+    "revision": "1dd964caa9ad17ab9d91",
+    "url": "/covid-19-reports-react/static/js/83.b151c9c3.chunk.js"
+  },
+  {
+    "revision": "e730aeb716189d203834",
+    "url": "/covid-19-reports-react/static/js/84.433a6b7a.chunk.js"
+  },
+  {
+    "revision": "dbdde21c8b9c0b7ce4ea",
+    "url": "/covid-19-reports-react/static/js/85.a67035c2.chunk.js"
+  },
+  {
+    "revision": "70827acdf47f3ce46ae1",
+    "url": "/covid-19-reports-react/static/js/86.1d235dd7.chunk.js"
+  },
+  {
+    "revision": "c1d51232e6ff4e68b0ca",
+    "url": "/covid-19-reports-react/static/js/87.4431a9d4.chunk.js"
+  },
+  {
+    "revision": "db7c34c9574053068e11",
+    "url": "/covid-19-reports-react/static/js/88.c32c873c.chunk.js"
+  },
+  {
+    "revision": "fa0139b2381cec2ad773",
+    "url": "/covid-19-reports-react/static/js/89.865877a4.chunk.js"
+  },
+  {
+    "revision": "6f17caf6f95fa167decc",
+    "url": "/covid-19-reports-react/static/js/9.48cd3fe7.chunk.js"
+  },
+  {
+    "revision": "a645dd46ecbb99526dd3",
+    "url": "/covid-19-reports-react/static/js/main.0579ed4f.chunk.js"
+  },
+  {
+    "revision": "4894a80cbfdfc4796b05",
+    "url": "/covid-19-reports-react/static/js/runtime-main.0ac6f659.js"
+  },
+  {
+    "revision": "c1ed66a35bae9dad14de791afe4f14df",
+    "url": "/covid-19-reports-react/static/media/bar-chart.c1ed66a3.svg"
+  },
+  {
+    "revision": "6453138d29edc90bbe3b414a8fe748ea",
+    "url": "/covid-19-reports-react/static/media/geo-alt.6453138d.svg"
+  },
+  {
+    "revision": "42f593f75f3adaeb2ba40d68caf4e5f6",
+    "url": "/covid-19-reports-react/static/media/heart-fill.42f593f7.svg"
+  },
+  {
+    "revision": "be4a3a856408b3ccd59f4f540ded7987",
+    "url": "/covid-19-reports-react/static/media/house-door-fill.be4a3a85.svg"
+  }
+]);
